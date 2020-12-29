3c1972d78336d1a5cfcf57d6e6f17fec27675664
chrispelt <53978868+chrispelt@users.noreply.github.com>
2020-12-29 13:53:30 +0200

    serial bitonic sort with timers

--- bitonic_ser.c
+++ bitonic_ser.c
@@ -4,7 +4,9 @@
 #include <time.h>
 #include <math.h>
 
-#define N 32  // must be a power of 2
+#include "mpi.h"
+
+#define N 4096  // must be a power of 2
 
 #define ascending 1
 #define descending 0
@@ -82,7 +84,9 @@ void bitonicSort(int arr[N], int cnt, int dir){
 
 
 int main(){
-
+	clock_t timer_start;
+	clock_t timer_end;
+	double exec_time = 0.0;
 	srand(time(0));
 	int a[N];
 
@@ -91,13 +95,20 @@ int main(){
 	}
 
 	//int a[] = {10, 2, 65, 9, 8, 5, 4, 32};
-
+	
+	timer_start = clock();
 	bitonicSort(a, N, ascending);
+	timer_end = clock();
+	
+	exec_time += (double)(timer_end - timer_start) / CLOCKS_PER_SEC;
 
+/*
 	for(int i=0; i<N; i++){
 		printf("%d ", a[i]);
 	}
 	printf("\n");
+*/
+	printf("Execution time: %f sec\n", exec_time);
 
 	return 0;
 
